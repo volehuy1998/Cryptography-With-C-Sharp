63e0e5df34174947937d5f81ab846d52f78a7b83
Võ Lê Huy <volehuy1998@gmail.com>
2020-11-08 22:25:20 +0700

    Update Common: modulo BigInteger

--- Common/MathHelpers.cs
+++ Common/MathHelpers.cs
@@ -3,6 +3,7 @@ using MathNet.Numerics.LinearAlgebra.Complex;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,11 @@ namespace Common
     public class MathHelpers
     {
         public static int Modulo(int a, int b)
+        {
+            return (a % b + b) % b;
+        }
+
+        public static BigInteger Modulo(BigInteger a, BigInteger b)
         {
             return (a % b + b) % b;
         }
