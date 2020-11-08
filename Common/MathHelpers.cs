5c56bec6bff6aae0dc9b95ff25d917e84e586ab3
Võ Lê Huy <volehuy1998@gmail.com>
2020-11-08 22:27:44 +0700

    Update Common: add GCD + EulerTotient

--- Common/MathHelpers.cs
+++ Common/MathHelpers.cs
@@ -20,5 +20,21 @@ namespace Common
         {
             return (a % b + b) % b;
         }
+
+        public static int GCD(int a, int b)
+        {
+            if (a == 0)
+                return b;
+            return GCD(b % a, a);
+        }
+
+        public static int EulerTotient(int n)
+        {
+            int result = 1;
+            for (int i = 2; i < n; i++)
+                if (GCD(i, n) == 1)
+                    result++;
+            return result;
+        }
     }
 }
