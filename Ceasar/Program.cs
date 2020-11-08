ff3c7a0c285754b45ee27cca25cb8ec7096b490a
Võ Lê Huy <volehuy1998@gmail.com>
2020-11-08 07:34:08 +0700

    Update Caesar comment

--- Ceasar/Program.cs
+++ Ceasar/Program.cs
@@ -21,6 +21,7 @@
                 int alphaIndex = alphabet.IndexOf(p);
                 if (alphaIndex >= 0)
                 {
+                    // c = (p + k) mod 26
                     cipher += alphabet[Modulo(alphaIndex + key, alphabet.Length)];
                 }
                 else
@@ -39,10 +40,10 @@
             for (int i = 0; i < cipher.Length; i++)
             {
                 char p = char.ToUpper(cipher[i]);
-                //int cipherIndex = key.IndexOf(p);
                 int cipherIndex = alphabet.IndexOf(p);
                 if (cipherIndex >= 0)
                 {
+                    // p = (c - k) mod 26
                     msg += alphabet[Modulo(cipherIndex - key, alphabet.Length)];
                 }
                 else
